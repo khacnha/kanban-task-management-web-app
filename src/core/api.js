37c2554b7f5d2d571d463bc40345547c9389f5ba
Nha <khacnha.it@gmail.com>
2023-05-17 18:59:50 +0700

    feature: and toast

--- src/core/api.js
+++ src/core/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const API_BASE_URL = "http://localhost:8000/api";
 
@@ -9,12 +11,26 @@ const api = axios.create({
   },
 });
 
+// Add a request interceptor
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Token ${token}`;
   }
   return config;
+});
+
+// Add a response interceptor
+api.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  console.log("APIerror", error.message);
+  toast.error(error.message);
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
 });
 
 export const auth = {
